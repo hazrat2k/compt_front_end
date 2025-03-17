575578ff668256ee1fb968d1cf78a08236ecb95b
Hazrat Ali <hazrat2k@gmail.com>
2025-03-17 15:14:58 +0600

    Notice Create

--- src/pages/notices/notices.jsx
+++ src/pages/notices/notices.jsx
@@ -1,15 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./notices.css";
+
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Download, Eye } from "lucide-react";
+
+import axios from "axios";
+import { backend_site_address } from "../../stores/const/siteAddress";
 
 import NavBar from "../../component/page_compo/navBar/navBar";
 import Footer from "../../component/page_compo/footer/footer";
 
-export default function Notices(){
-    return(
+export default function Notices() {
+    const [noticeList, setNoticeList] = useState([]);
+
+    const getDirectDownloadLink = (driveLink) => {
+        const match = driveLink.match(/\/d\/(.*)\/view/);
+        return match
+            ? `https://drive.google.com/uc?export=download&id=${match[1]}`
+            : driveLink;
+    };
+
+    const getPreviewLink = (driveLink) => {
+        const match = driveLink.match(/\/d\/(.*)\/view/);
+        return match
+            ? `https://drive.google.com/file/d/${match[1]}/preview`
+            : driveLink;
+    };
+
+    const loadNotice = async () => {
+        try {
+            const noticeRes = await axios.get(
+                "http://" + backend_site_address + "/get_notices"
+            );
+            setNoticeList(noticeRes.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        loadNotice();
+    }, []);
+
+    return (
         <>
-            <NavBar hide={{nav_mid: true}} />
-            <div>
-                Notices
+            <NavBar hide={{ nav_mid: true }} />
+            <div className="download_page">
+                <div className="download_page_heading">Notices</div>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Notice No</TableCell>
+                            {/* <TableCell>Description</TableCell> */}
+                            <TableCell>Link</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {noticeList.map((notice) => (
+                            <TableRow key={notice.NOTICE_NO}>
+                                <TableCell>{notice.NOTICE_NO}</TableCell>
+                                {/* <TableCell>{file.size}</TableCell> */}
+                                {/* <TableCell>
+                                    <a
+                                        href={getDirectDownloadLink(
+                                            notice.LINK_ADDRESS
+                                        )}
+                                        download
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        <Button
+                                            variant="outlined"
+                                            startIcon={<Download size={16} />}
+                                        >
+                                            Download
+                                        </Button>
+                                    </a>
+                                </TableCell> */}
+                                <TableCell>
+                                    <Button
+                                        variant="outlined"
+                                        startIcon={<Eye size={16} />}
+                                        onClick={() =>
+                                            window.open(
+                                                getPreviewLink(
+                                                    notice.LINK_ADDRESS
+                                                ),
+                                                "_blank"
+                                            )
+                                        }
+                                    >
+                                        Preview
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
 
             <Footer />
--- src/stores/const/siteAddress.js
+++ src/stores/const/siteAddress.js
@@ -1,1 +1,2 @@
-export const backend_site_address = "172.16.0.124:8800";
+// export const backend_site_address = "172.16.0.124:8800";
+export const backend_site_address = "10.15.141.23:8800";
